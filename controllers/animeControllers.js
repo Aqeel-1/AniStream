58530380b6096abf68c7a1e27d1c7c6f61659f59
NULL <aqeelprogrammer@gmail.com>
2024-09-25 23:24:37 +0300

    Add Anime model and implementing all CRUD operations in anime controllers

--- controllers/animeControllers.js
+++ controllers/animeControllers.js
@@ -2,9 +2,61 @@
  * this file contain all controllers for handling anime requests
  */
 
-exports.getAllAnimes = (req, res, next) => {
+const Anime = require('../models/animeModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.getAllAnimes = async (req, res, next) => {
+  const features = new APIFeatures(Anime.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .sort('animeReleaseDate')
+    .paginate();
+
+  const animes = await features.query;
   res.status(200).json({
     status: 'success',
-    data: null,
+    results: animes.length,
+    data: animes,
+  });
+};
+
+exports.createAnime = async (req, res, next) => {
+  const newAnime = await Anime.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      newAnime,
+    },
+  });
+};
+
+exports.getAnime = async (req, res, next) => {
+  const anime = await Anime.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: anime,
+  });
+};
+
+exports.updateAnime = async (req, res, next) => {
+  const updatedAnime = await Anime.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    data: updatedAnime,
+  });
+};
+
+exports.deleteAnime = async (req, res, next) => {
+  const deletedAnime = await Anime.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: deletedAnime,
   });
 };
--- routes/animeRoutes.js
+++ routes/animeRoutes.js
@@ -9,5 +9,14 @@ const animeControllers = require('../controllers/animeControllers');
 const router = express.Router();
 
 // routes
-router.route('/').get(animeControllers.getAllAnimes);
+router
+  .route('/')
+  .get(animeControllers.getAllAnimes)
+  .post(animeControllers.createAnime);
+
+router
+  .route('/:id')
+  .get(animeControllers.getAnime)
+  .patch(animeControllers.updateAnime)
+  .delete(animeControllers.deleteAnime);
 module.exports = router;
